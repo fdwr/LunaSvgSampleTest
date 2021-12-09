a20cc4bb65b74b2df8e16a1aae08676e350ed6ed
Dwayne Robinson <dwayner@microsoft.com>
2021-12-09 05:01:52 -0800

    Clean old duplicates

--- plain/test/plaintest (merge)/builddebug.bat
+++
@@ -1,4 +0,0 @@
-::Include the Pgfx object files.
-::Exclude the definitions, otherwise the linker complains about multiply defined symbols.
-::Set version to Unicode (maybe should use MSLU)
-dbuild plaintest.d @build.brf -debug -g -L/DEBUGLI/DEBUGP/DEBUGLO %1 %2 %3 %4 %5 %6 %7 %8 %9
