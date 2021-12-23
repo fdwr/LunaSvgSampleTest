768062c23f12c593f01f2c921990ab196ad2bb84
Dwayne Robinson <dwayner@microsoft.com>
2021-12-23 04:23:17 -0800

    Tilemap Viewer update sequence data

--- TileMapViewer/testdata/GenerateSequence.py
+++ TileMapViewer/testdata/GenerateSequence.py
@@ -6,50 +6,36 @@ if sys.byteorder == "big":
 #endif
 
 totalCount = 64 * 64
-totalRange = range(totalCount)
 
-def WriteSequenceToFile(outputArray: array, typeMask: int, step: int, fileName: str):
+def WriteSequenceToFile(outputArray: array, totalCount: int, typeMask: int, orMask: int, step: int, fileName: str):
     with open(fileName, "wb") as f:
-        for i in totalRange:
-            outputArray.append((i * step) & typeMask)
+        for i in range(totalCount):
+            outputArray.append(((i * step) | orMask) & typeMask)
         #endfor
         outputArray.tofile(f)
     #endwith
 #enddef
 
-WriteSequenceToFile(array.array("B"), 255, 1, "SequenceUint8.bin")
-WriteSequenceToFile(array.array("H"), 65535, 1, "SequenceUint16.bin")
-WriteSequenceToFile(array.array("I"), 0xFFFFFFFF, 1, "SequenceUint32.bin")
+def Write2DSequenceToFile(outputArray: array, xCount: int, yCount: int, typeMask: int, orMask: int, step: int, fileName: str):
+    with open(fileName, "wb") as f:
+        for y in range(yCount):
+            for x in range(xCount):
+                outputArray.append(((x * step) | ((y << 8) * step) | orMask) & typeMask)
+            #endfor
+        #endfor
+        outputArray.tofile(f)
+    #endwith
+#enddef
 
-WriteSequenceToFile(array.array("B"), 255, 8, "SequenceUint8Step8.bin")
-WriteSequenceToFile(array.array("H"), 65535, 8, "SequenceUint16Step8.bin")
-WriteSequenceToFile(array.array("I"), 0xFFFFFFFF, 8, "SequenceUint32Step8.bin")
+WriteSequenceToFile(array.array("B"), totalCount, 255, 0, 1, "SequenceUint8.bin")
+WriteSequenceToFile(array.array("H"), totalCount, 65535, 0, 1, "SequenceUint16.bin")
+WriteSequenceToFile(array.array("I"), totalCount, 0xFFFFFFFF, 0xFF000000, 1, "SequenceUint32.bin")
 
-#WriteSequenceToFile(array.array("B"), "SequenceUint8.bin")
-#WriteSequenceToFile(array.array("H"), "SequenceUint8.bin")
-#WriteSequenceToFile(array.array("I"), "SequenceUint8.bin")
-#with open(, "wb") as f:
-#    for i in totalRange:
-#        outputArray.append(i & 255)
-#    #endfor
-#    outputArray.tofile(f)
-##endwith
-#
-#outputArray = array.array("H")
-#with open("SequenceUint16.bin", "wb") as f:
-#    for i in totalRange:
-#        outputArray.append(i & 65535)
-#    #endfor
-#    outputArray.tofile(f)
-##endwith
-#
-#outputArray = array.array("I")
-#with open("SequenceUint32.bin", "wb") as f:
-#    for i in totalRange:
-#        outputArray.append(i)
-#    #endfor
-#    outputArray.tofile(f)
-##endwith
+WriteSequenceToFile(array.array("B"), totalCount, 255, 0, 4, "SequenceUint8Step4.bin")
+WriteSequenceToFile(array.array("H"), totalCount, 65535, 0, 4, "SequenceUint16Step4.bin")
+WriteSequenceToFile(array.array("I"), totalCount, 0xFFFFFFFF, 0xFF000000, 4, "SequenceUint32Step4.bin")
+
+Write2DSequenceToFile(array.array("I"), 16, 16, 0xFFFFFFFF, 0xFF000000, 17, "Sequence2DUint32Step16.bin")
 
 # Litter code:
 #f.write(i.to_bytes(1, byteorder="little"))
