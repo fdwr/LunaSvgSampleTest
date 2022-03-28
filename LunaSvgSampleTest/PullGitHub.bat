9e2e76e815a7bdafc104838583fae2b9d19da491
Dwayne Robinson <fdwr@hotmail.com>
2022-03-27 17:54:40 -0700

    LunaSvgTest update subtree pull and push

--- LunaSvgSampleTest/PullGitHub.bat
+++ LunaSvgSampleTest/PullGitHub.bat
@@ -5,7 +5,7 @@ setlocal
 :: "You need to run this command from the toplevel of the working tree."
 pushd %~dp0\..
 
-set command=git subtree pull --prefix LunaSvgTest https://github.com/fdwr/LunaSvgSampleTest.git master
+set command=git subtree pull --prefix LunaSvgSampleTest https://github.com/fdwr/LunaSvgSampleTest.git master
 echo %command%
 %command%
 
