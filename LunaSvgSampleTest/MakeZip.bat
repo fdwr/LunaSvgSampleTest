02c4c94263cbae5c83de1814bbd999097bfe406c
Dwayne Robinson <fdwr@hotmail.com>
2022-04-27 18:32:07 -0700

    LunaSvgTest add sample icons from icons8 fluency set

--- LunaSvgSampleTest/MakeZip.bat
+++ LunaSvgSampleTest/MakeZip.bat
@@ -1,4 +1,11 @@
-::setlocal
-::set fileName=build\x86-release\LunaSvgTest.pikensoft.%date%.zip
-del build\x86-release\LunaSvgTest.pikensoft.*.zip 2> nul
-\programs\file\7-Zip\7zG.exe a -tzip -mcu=on -mx=9 %filename% ./Readme.md ./License.txt .\build\x86-release\LunaSvgTest.exe "svgs\iconpacks.net maneki-neko-9048 by Yusuf Onaldi.*" "svgs\reshot.com thedighital icon-scottish-female-ENH4GZFT72.*"
+@echo off
+setlocal
+set oldFileNames=build\x86-release\LunaSvgTest.pikensoft.*.zip
+set  newFileName=build\x86-release\LunaSvgTest.pikensoft.%date%.zip
+
+echo Deleting any old files: %oldFileNames%
+del %oldFileNames% 2> nul
+echo Compressing new file: %newFileName%
+set command=\programs\file\7-Zip\7zG.exe a -tzip -mcu=on -mx=9 %newFileName% ./Readme.md ./License.txt .\build\x86-release\LunaSvgTest.exe "svgs\iconpacks.net maneki-neko-9048 by Yusuf Onaldi.*" "svgs\reshot.com thedighital icon-scottish-female-ENH4GZFT72.*" "svgs\icons8.com fluency home.svg" "svgs\icons8.com fluency picture.svg" "svgs\icons8.com fluency scroll.svg" "svgs\icons8.com fluency popular set.url" "svgs\*license.txt"
+echo %command%
+%command%
