280207deb54776204222bd99d97677767da3af84
Dwayne Robinson <fdwr@hotmail.com>
2022-06-29 16:45:51 -0700

    LunaSvgTest update PushGitHub.bat to execute much faster due to subtree split before push

--- LunaSvgSampleTest/PushGitHub.bat
+++ LunaSvgSampleTest/PushGitHub.bat
@@ -5,7 +5,18 @@ setlocal
 :: "You need to run this command from the toplevel of the working tree."
 pushd %~dp0\..
 
-set command=git subtree push --prefix LunaSvgSampleTest https://github.com/fdwr/LunaSvgSampleTest.git master
+:: Normally we'd use subtree push, but that takes longer and longer the more commits there are in the repo,
+:: as git rereads the whole history to split out any changes under that directory. By instead splitting the
+:: repo and then pushing that branch (and keeping the branch around), it takes barely longer than a normal
+:: push command.
+
+set command=git subtree split --rejoin --prefix LunaSvgSampleTest -b LunaSvgSampleTest
+echo %command%
+%command%
+
+::Avoid subtree push, instead pushing from the split branch.
+:: set command=git subtree push --prefix LunaSvgSampleTest https://github.com/fdwr/LunaSvgSampleTest.git master
+set command=git push https://github.com/fdwr/LunaSvgSampleTest.git LunaSvgSampleTest:master
 echo %command%
 %command%
 
