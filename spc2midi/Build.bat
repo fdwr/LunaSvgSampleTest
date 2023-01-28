40cd9c685f43e93f98e58cfcb1a97ca85ec7bff3
Dwayne Robinson <fdwr@hotmail.com>
2023-01-28 03:08:46 -0800

    Spc2Midi fix build.bat

--- spc2midi/Build.bat
+++ spc2midi/Build.bat
@@ -3,20 +3,23 @@
 :: Link object file and resources into PE
 
 setlocal
-path %path%;\src\bin
+path %path%;..\tools
 
 set ErrorBuilding=0
-echo Compiling into Win32 COFF with Nasm... (with resources)
 pushd src
+
+echo Compiling into Win32 COFF with Nasm... (with resources)
 nasmw.exe -fwin32 Spc2Midi.asm -o temp.obj -w+orphan-labels -dWinVer -d_NOMSLINK -dGuiVer -iWIN\ -iGUI\ %1 %2 %3 %4 %5 %6 %7 %8 %9
 IF ERRORLEVEL 1 set ErrorBuilding=1
-popd
-
-if ErrorBuilding goto End
+if %ErrorBuilding%==1 goto End
 
 set RSPFILE=
 if exist SPC2MIDI.RSP set RSPFILE=@SPC2MIDI.RSP
 
-ALink.exe -oPE src\temp.obj win\WinImp.lib Spc2Midi.res -o Spc2Midi.exe %RSPFILE% -entry Main
-del temp.obj 2> nul
+echo Linking with ALink...
+ALink.exe -oPE temp.obj win\WinImp.lib Spc2Midi.res -o ..\Spc2Midi.exe %RSPFILE% -entry Main
+
 :End
+
+del temp.obj 2> nul
+popd
